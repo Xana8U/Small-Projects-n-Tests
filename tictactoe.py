10db4b460b480241704f5669c4a25dde97cba2b2
xana8u <xana8u@gmail.com>
2017-12-15 23:51:21 +0200

    got is_won function rules to trigger, next adding game finish on victory and a new game start

--- tictactoe.py
+++ tictactoe.py
@@ -12,6 +12,7 @@ allowed_keys = [113, 119, 101, 97, 115, 100, 122, 120, 99]
 lastturn = "oturn"
 
 
+# Controls the displayed keyboard and turns
 def gameboard(board):
         print("\n")
         is_won(board)
@@ -24,35 +25,38 @@ def gameboard(board):
             oturn(mygetch())
 
 
+# Captures keypresses for x and oturn
 def mygetch():
     playerchoice = ord(msvcrt.getch())
     return playerchoice
 
 
+# Checks if one of the players has won the game
 def is_won(board):
-    if board[3] and board[7] and board[11] is "X" or board[3] and board[7] and board[11] is "O":  # top horizontal
+    if board[2] == "X" and board[6] == "X" and board[10] == "X" or board[2] == "O" and board[6] == "O" and board[10] == "O":  # top horizontal
         print("CONGRATULATIONS!!")
-    elif board[16] and board[20] and board[24] is "X" or board[16] and board[20] and board[24] is "O":  # middle horizontal
+    elif board[15] == "X" and board[19] == "X" and board[23] == "X" or board[15] == "O" and board[19] == "O" and board[23] == "O":  # middle horizontal
         print("CONGRATULATIONS!!")
-    elif board[29] and board[33] and board[37] is "X" or board[29] and board[33] and board[37] is "O":  # bottom horizontal
+    elif board[28] == "X" and board[32] == "X" and board[36] == "X" or board[28] == "O" and board[32] == "O" and board[36] == "O":  # bottom horizontal
         print("CONGRATULATIONS!!")
-    elif board[3] and board[16] and board[29] is "X" or "O":  # left vertical
+    elif board[2] == "X" and board[15] == "X" and board[28] == "X" or board[2] == "O" and board[15] == "O" and board[28] == "O":  # left vertical
         print("CONGRATULATIONS!!")
-    elif board[7] and board[20] and board[33] is "X" or "O":  # middle vertical
+    elif board[6] == "X" and board[19] == "X" and board[32] == "X" or board[6] == "O" and board[19] == "O" and board[32] == "O":  # middle vertical
         print("CONGRATULATIONS!!")
-    elif board[11] and board[24] and board[37] is "X" or "O":  # right vertical
+    elif board[10] == "X" and board[23] == "X" and board[36] == "X" or board[10] == "O" and board[23] == "O" and board[36] == "O":  # right vertical
         print("CONGRATULATIONS!!")
-    elif board[3] and board[20] and board[37] is "X" or "O":  # left top - right bottom
+    elif board[2] == "X" and board[19] == "X" and board[36] == "X" or board[2] == "O" and board[19] == "O" and board[36] == "O":  # left top - right bottom
         print("CONGRATULATIONS!!")
-    elif board[11] and board[20] and board[29] is "X" or "O":  # right top - left bottom
+    elif board[10] == "X" and board[19] == "X" and board[28] == "X" or board[10] == "O" and board[19] == "O" and board[28] == "O":  # right top - left bottom
         print("CONGRATULATIONS!!")
     else:
-        return 0
+        return
 
 
 def is_occupied():
 
 
+# Place X on player chosen position
 def xturn(playerchoice): # box move for slices is +4 row change is +1
     global board
     global lastturn
@@ -61,53 +65,54 @@ def xturn(playerchoice): # box move for slices is +4 row change is +1
         os.system("cls")
         lastturn = "xturn"
         gameboard(board)
-    if playerchoice == 119:  # W
+    elif playerchoice == 119:  # W
         board = board[:6] + "X" + board[7:]
         os.system("cls")
         lastturn = "xturn"
         gameboard(board)
-    if playerchoice == 101:  # E
+    elif playerchoice == 101:  # E
         board = board[:10] + "X" + board[11:]
         os.system("cls")
         lastturn = "xturn"
         gameboard(board)
-    if playerchoice == 97:  # A
+    elif playerchoice == 97:  # A
         board = board[:15] + "X" + board[16:]
         os.system("cls")
         lastturn = "xturn"
         gameboard(board)
-    if playerchoice == 115:  # S
+    elif playerchoice == 115:  # S
         board = board[:19] + "X" + board[20:]
         os.system("cls")
         lastturn = "xturn"
         gameboard(board)
-    if playerchoice == 100:  # D
+    elif playerchoice == 100:  # D
         board = board[:23] + "X" + board[24:]
         os.system("cls")
         lastturn = "xturn"
         gameboard(board)
-    if playerchoice == 122:  # Z
+    elif playerchoice == 122:  # Z
         board = board[:28] + "X" + board[29:]
         os.system("cls")
         lastturn = "xturn"
         gameboard(board)
-    if playerchoice == 120:  # X
+    elif playerchoice == 120:  # X
         board = board[:32] + "X" + board[33:]
         os.system("cls")
         lastturn = "xturn"
         gameboard(board)
-    if playerchoice == 99:  # C
+    elif playerchoice == 99:  # C
         board = board[:36] + "X" + board[37:]
         os.system("cls")
         lastturn = "xturn"
         gameboard(board)
-    if playerchoice not in allowed_keys:
+    elif playerchoice not in allowed_keys:
         sys.stderr("INCORRECT INPUT PLEASE TRY AGAIN!!!")
         time.sleep(1)
         os.system("cls")
         xturn(mygetch())
 
 
+# Places O on player chosen location
 def oturn(playerchoice):
     global board
     global lastturn
